1e84eaa3b40f21a884d211d5fc0297c967eef3fc
rawstorage <paule.martin@dell.com>
2018-12-12 14:36:00 +0000

    Final submitted version for Ansible.  pep8 compliant and ready for push.

--- library/dellpmax_createhostgroup.py
+++ library/dellpmax_createhostgroup.py
@@ -1,107 +1,94 @@
 #!/usr/bin/python
-
+# Copyright (C) 2018 DellEMC
+# Author(s): Paul Martin <paule.martin@dell.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
-import PyU4V
-
 __metaclass__ = type
 
-ANSIBLE_METADATA = {'metadata_version': '1.0',
-                    'status': ['preview'],
-                    'supported_by': 'VMAX REST API Community '
-                                    'https://community.emc.com/docs/DOC-56447'
-                    }
-DOCUMENTATION = r'''
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
 ---
-module: dellpmax_createsg
-
-Author: Paul Martin @rawstorage
-
-Contributors: Rob Mortell @robmortell
-
-software versions=ansible 2.6.2
-                  python version = 2.7.15rc1 (default, Apr 15 2018,
-                  PyU4V v3.0.15
-                  
-short_description: 
-    Module to Create Host Group for Masking Storage to a Cluster, 
-    host groups are collections of hosts.  This module requires that hosts 
-    have already been created to be added.  Use dellpmax_createhost task in 
-    your playbook to create hosts in advance. 
-
-notes:
-    - This module has been tested against UNI 9.0.  Every effort has been 
-    made to verify the scripts run with valid input.  These modules 
-    are a tech preview.  Additional error handling will be added at a later 
-    date, base functionality only right now.
-
-
-
-Requirements:
-    - Ansible, Python 2.7, Unisphere for PowerMax version 9.0 or higher. 
-    VMAX All Flash, VMAX3, or PowerMAX storage Array. Python module PyU4V 
-    also needs to be installed from pip or PyPi
-    
-
-
-
-playbook options:
-    unispherehost:
-        description:
-            - Full Qualified Domain Name or IP address of Unisphere for 
-            PowerMax host.
-        required:True
-
-    universion:
-        -description:
-            - Integer, version of unipshere software 
-            https://{HostName|IP}:8443/univmax/restapi/{version}/{resource}
-            90 is the release at time of writing module.
-        -required:True
-    verifycert:
-        description: 
-            -Boolean, securitly check on ssl certificates
-        required:True             
-
-        required: True
-    array_id:
-        description:
-            - Integer 12 Digit Serial Number of PowerMAX or VMAX array.
-        required:True
-    host_id:
-         description:
-            -  String value to denote hostname, No  Special Character 
-            support except for _.  Case sensistive for REST Calls.
-        required:True
-    initiator_list:
-        description:
-            - List of Initiator WWN or IQN 
-        required:True
-    consistent_lun:
-        description:
-            - Boolean Value, specifying consistent_lun ensures LUN address 
-            consistency across ports, this is not required on most modern 
-            operating systems as WWN or UUID is used to Uniqely identify luns
-        required:True    
-    async:
-        Optional Parameter to set REST call to run Asyncronously, job will 
-        be submitted to job queue and executed.  Task Id will be returned in 
-        JSON for lookup purposed to check job completion status. 
-
+author:
+  - "Paul Martin (@rawstorage)"
+short_description: "Create storage group on Dell EMC PowerMax or VMAX All
+Flash"
+version_added: "2.8"
+description:
+  - "This module has been tested against UNI 9.0. Every effort has been made
+  to verify the scripts run with valid input. These modules are a tech preview"
+module: dellpmax_createhostgroup
+options:
+  array_id:
+    description:
+      - "Integer 12 Digit Serial Number of PowerMAX or VMAX array."
+    required: true
+  unispherehost:
+    description:
+      - "Fully Qualified Domain Name or IP address of Unisphere for PowerMax
+      host."
+    required: true
+  universion:
+    description:
+      - "Integer, version of unipshere software  e.g. 90"
+    required: true
+  verifycert:
+    description:
+      - "Boolean, security check on ssl certificates"
+    type: bool
+    required: true
+  vol_size:
+    description:
+      - "Integer value for the size of volumes.  All volumes will be created
+      with same size.  Use dellpmax_addvol to add additional volumes if you
+      require different sized volumes once storage group is created."
+    required: true
+  volumeIdentifier:
+    description:
+      - "String up to 64 Characters no special character other than
+      underscore sets a label to make volumes easily identified on hosts can
+      run Dell EMC inq utility command to see this label is  inq -identifier
+      device_name"
+    required: false
+  user:
+    description:
+      - "Unisphere username"
+  password:
+    description:
+      - "password for Unisphere user"
+  cluster_name:
+    description:
+      - "32 Character string no special character permitted except for
+      underscore"
+  host_list:
+    description:
+      - "List of Hosts to be added to the Cluster.  Hosts must already exist
+      for task to complete, will error out"
+requirements:
+  - Ansible
+  - "Unisphere for PowerMax version 9.0 or higher."
+  - "VMAX All Flash, VMAX3, or PowerMax storage Array."
+  - "PyU4V version 3.0.0.8 or higher using PIP python -m pip install PyU4V"
 '''
-
-EXAMPLES = r'''
-- name: Create Cluster
-  hosts: localhost
+EXAMPLES = '''
+---
+- name: "Add volumes to existing storage group"
   connection: local
+  hosts: localhost
   vars:
-        unispherehost: '192.168.20.63'
-        uniport: 8443
-        universion: "90"
-        verifycert: False
-        user: 'smc'
-        password: 'smc'
-        array_id: "000197600123"
+    array_id: 000197600156
+    password: smc
+    sgname: Ansible_SG
+    unispherehost: "192.168.1.123"
+    universion: "90"
+    user: smc
+    verifycert: false
+
   tasks:
     dellpmax_createhostgroup:
              unispherehost: "{{unispherehost}}"
@@ -118,7 +105,7 @@ EXAMPLES = r'''
 '''
 RETURN = r'''
 '''
-
+from ansible.module_utils.basic import AnsibleModule
 
 def main():
     changed = False
@@ -136,7 +123,15 @@ def main():
 
         )
     )
-    # Make REST call to Unisphere Server and execute create Host
+    try:
+        import PyU4V
+    except:
+        module.fail_json(
+            msg='Requirements not met PyU4V is not installed, please install'
+                'via PIP')
+        module.exit_json(changed=changed)
+
+    # Make REST call to Unisphere Server and execute create Host Group
 
     payload = (
         {
@@ -158,7 +153,7 @@ def main():
 
     dellemc = conn.provisioning
 
-    changed = False
+
     # Check for each host in the host list that it exists, otherwise fail
     # module.
 
@@ -187,5 +182,3 @@ from ansible.module_utils.urls import *
 
 if __name__ == '__main__':
     main()
-
-
