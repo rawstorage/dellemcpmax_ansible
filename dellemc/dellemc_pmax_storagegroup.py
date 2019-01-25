503a8af350908b1925b67295d465cc6b63c426e6
rawstorage <paule.martin@dell.com>
2019-01-25 10:58:43 +0000

    updated examples and returns.  Tested end to end.

--- dellemc/dellemc_pmax_storagegroup.py
+++ dellemc/dellemc_pmax_storagegroup.py
@@ -89,19 +89,39 @@ requirements:
 '''
 EXAMPLES = '''
 ---
-- name: "Delete Storage Group"
+- name: "Test Storage Group Operations"
   connection: local
   hosts: localhost
   vars:
     array_id: "000197600156"
     password: "smc"
-    sgname: "Ansible_SG"
-    unispherehost: "192.168.1.1"
+    sgname: "Ansible_SG1"
+    unispherehost: "10.60.156.63"
     universion: "90"
     user: "smc"
     verifycert: false
+    lun_list:
+      - cap_unit: "GB"
+        num_vols: 2
+        vol_size: 1
+        vol_name: "DATA"
+      - cap_unit: "GB"
+        num_vols: 2
+        vol_size: 1
+        vol_name: "REDO"
+    new_lun_list:
+      - cap_unit: "GB"
+        num_vols: 2
+        vol_size: 1
+        vol_name: "FRA"
+      - cap_unit: "GB"
+        num_vols: 2
+        vol_size: 1
+        vol_name: "TEMP"
+    lun_removal_list:
+      - "001A2"
   tasks:
-    - name: "Delete Existing Storage Group"
+    - name: "Create New Storage Group volumes"
       dellemc_pmax_storagegroup:
         array_id: "{{array_id}}"
         password: "{{password}}"
@@ -112,31 +132,10 @@ EXAMPLES = '''
         universion: "{{universion}}"
         user: "{{user}}"
         verifycert: "{{verifycert}}"
-        action: "delete"
-    - debug: var=storagegroup_detail
+        luns: "{{lun_list}}"
+        action: create
+    - debug: var=storagegroup_detail    
 
-- name: "Add New Volumes to Existing Storage Group"
-  connection: local
-  hosts: localhost
-  vars:
-    array_id: "000197600156"
-    password: "smc"
-    sgname: "Ansible_SG1"
-    unispherehost: "192.168.1.1"
-    universion: "90"
-    user: "smc"
-    verifycert: false
-    new_lun_list:
-      - cap_unit: "GB"
-        num_vols: 2
-        vol_size: 1
-        vol_name: "DATA"
-      - cap_unit: "GB"
-        num_vols: 2
-        vol_size: 1
-        vol_name: "REDO"
-
-  tasks:
     - name: "Add Storage Group volumes"
       dellemc_pmax_storagegroup:
         array_id: "{{array_id}}"
@@ -152,85 +151,6 @@ EXAMPLES = '''
         action: "add_volumes"
     - debug: var=storagegroup_detail
 
-- name: "Remove Volumes from Stroage Group"
-  connection: local
-  hosts: localhost
-  vars:
-    array_id: "000197600156"
-    password: "smc"
-    sgname: "Ansible_SG1"
-    unispherehost: "192.168.1.1"
-    universion: "90"
-    user: "smc"
-    verifycert: false
-    lun_list:
-      - 0019A
-      - 0019D
-  tasks:
-    - name: "Remove Volume from Storage Group"
-      dellemc_pmax_storagegroup:
-        array_id: "{{array_id}}"
-        password: "{{password}}"
-        sgname: "{{sgname}}"
-        slo: "Diamond"
-        srp_id: "SRP_1"
-        unispherehost: "{{unispherehost}}"
-        universion: "{{universion}}"
-        user: "{{user}}"
-        verifycert: "{{verifycert}}"
-        luns: "{{lun_list}}"
-        action: remove_volumes
-    - debug: var=storagegroup_detail
-
-- name: "Create New Storage Group"
-  connection: local
-  hosts: localhost
-  vars:
-    array_id: "000197600156"
-    password: "smc"
-    sgname: "Ansible_SG1"
-    unispherehost: "192.168.1.1"
-    universion: "90"
-    user: "smc"
-    verifycert: false
-    new_lun_list:
-      - cap_unit: "GB"
-        num_vols: 2
-        vol_size: 1
-        vol_name: "DATA"
-      - cap_unit: "GB"
-        num_vols: 2
-        vol_size: 1
-        vol_name: "REDO"
-
-  tasks:
-    - name: "Create New Storage Group volumes"
-      dellemc_pmax_storagegroup:
-        array_id: "{{array_id}}"
-        password: "{{password}}"
-        sgname: "{{sgname}}"
-        slo: "Diamond"
-        srp_id: "SRP_1"
-        unispherehost: "{{unispherehost}}"
-        universion: "{{universion}}"
-        user: "{{user}}"
-        verifycert: "{{verifycert}}"
-        luns: "{{new_lun_list}}"
-        action: create
-    - debug: var=storagegroup_detail
-
-- name: "Show Storage Group Details and Volumes"
-  connection: local
-  hosts: localhost
-  vars:
-    array_id: "000197600156"
-    password: "smc"
-    sgname: "Ansible_SG1"
-    unispherehost: "10.60.156.63"
-    universion: "90"
-    user: "smc"
-    verifycert: false
-  tasks:
     - name: "Show Storage Group"
       dellemc_pmax_storagegroup:
         array_id: "{{array_id}}"
@@ -245,6 +165,170 @@ EXAMPLES = '''
         action: "show"
     - debug: var=storagegroup_detail
 
+    - name: "Remove volumes"
+      dellemc_pmax_storagegroup:
+        array_id: "{{array_id}}"
+        password: "{{password}}"
+        sgname: "{{sgname}}"
+        slo: "Diamond"
+        srp_id: "SRP_1"
+        unispherehost: "{{unispherehost}}"
+        universion: "{{universion}}"
+        user: "{{user}}"
+        verifycert: "{{verifycert}}"
+        luns: "{{lun_remove_list}}"
+        action: remove_volumes
+    - debug: var=storagegroup_detail
+
+    - name: "Delete Existing Storage Group"
+      dellemc_pmax_storagegroup:
+        array_id: "{{array_id}}"
+        password: "{{password}}"
+        sgname: "{{sgname}}"
+        slo: "Diamond"
+        srp_id: "SRP_1"
+        unispherehost: "{{unispherehost}}"
+        universion: "{{universion}}"
+        user: "{{user}}"
+        verifycert: "{{verifycert}}"
+        action: "delete"
+    - debug: var=storagegroup_detail
+'''
+RETURN = r'''
+Create Storage Group
+ok: [localhost] => {
+    "storagegroup_detail": {
+        "sg_volumes": [
+            "001A2",
+            "001A3",
+            "001A4",
+            "001A5"
+        ],
+        "storagegroup_name": "Ansible_SG1"
+    }
+}
+
+Add Volumes to Strogae Group
+
+ok: [localhost] => {
+    "storagegroup_detail": {
+        "message": "Operation Successful",
+        "new_volumes": [
+            "001AC",
+            "001AB",
+            "001AA",
+            "001AD"
+        ],
+        "sgdetails": {
+            "VPSaved": "100.0%",
+            "base_slo_name": "Diamond",
+            "cap_gb": 11.02,
+            "compression": true,
+            "device_emulation": "FBA",
+            "num_of_child_sgs": 0,
+            "num_of_masking_views": 0,
+            "num_of_parent_sgs": 0,
+            "num_of_snapshots": 0,
+            "num_of_vols": 11,
+            "service_level": "Diamond",
+            "slo": "Diamond",
+            "slo_compliance": "STABLE",
+            "srp": "SRP_1",
+            "storageGroupId": "Ansible_SG1",
+            "type": "Standalone",
+            "unprotected": true,
+            "vp_saved_percent": 100.0
+        },
+        "storagegroup_name": "Ansible_SG1"
+    }
+}
+
+Remove Volume
+
+ok: [localhost] => {
+    "storagegroup_detail": {
+        "message": "Volumes Sucessfully Removed"
+    }
+}
+
+Show Storage Group
+
+*
+ok: [localhost] => {
+    "storagegroup_detail": {
+        "message": "No changes made",
+        "sgdetails": {
+            "VPSaved": "100.0%",
+            "base_slo_name": "Diamond",
+            "cap_gb": 11.02,
+            "compression": true,
+            "device_emulation": "FBA",
+            "num_of_child_sgs": 0,
+            "num_of_masking_views": 0,
+            "num_of_parent_sgs": 0,
+            "num_of_snapshots": 0,
+            "num_of_vols": 11,
+            "service_level": "Diamond",
+            "slo": "Diamond",
+            "slo_compliance": "STABLE",
+            "srp": "SRP_1",
+            "storageGroupId": "Ansible_SG1",
+            "type": "Standalone",
+            "unprotected": true,
+            "vp_saved_percent": 100.0
+        },
+        "volumes": [
+            "001A3",
+            "001A4",
+            "001A5",
+            "001A6",
+            "001A7",
+            "001A8",
+            "001A9",
+            "001AA",
+            "001AB",
+            "001AC",
+            "001AD"
+        ]
+    }
+}
+
+Delete Stroage Group 
+
+ok: [localhost] => {
+    "storagegroup_detail": {
+        "message": "Delete Operation Completed",
+        "storagegroups": [
+            "Child3_RM_1",
+            "CriticalDatabase_1",
+            "Demo_GK",
+            "esx142_gks",
+            "esx142_gks_1",
+            "ESX_32_GKs",
+            "ESXi_DEMO_SG",
+            "FinanceOracle",
+            "HSBC",
+            "KMR-CSG",
+            "Lab_GK",
+            "MF_TEST",
+            "MTS_ESX_28",
+            "MTS_ESX_30",
+            "MTS_ESX_32",
+            "MTS_ESX_40",
+            "MyMetroSG",
+            "Pat_VdBench_1",
+            "Pat_VdBench_2",
+            "Pat_VdBench_3",
+            "Pat_VdBench_4",
+            "paul_csg2",
+            "PM_DELL40_GK",
+            "POC_Kit",
+            "SRDF_TEST",
+            "test1234",
+            "Uniprod_MGMT"
+        ]
+    }
+}
 
 
 '''
@@ -327,7 +411,7 @@ def add_volumes(apiconnection, module):
         sgvols_after = dellemc.get_volume_list(filters={
             'storageGroupId': module.params['sgname']})
         newvols = (list(set(sgvols_after) - set(sgvols_before)))
-
+        message = "Operation Successful"
     facts = ({'message': message,'storagegroup_name': module.params['sgname'],
               'new_volumes': newvols, 'sgdetails': dellemc.get_storage_group(
         storage_group_name=module.params['sgname'])})
